90447c8dd875050f3a8b6e9555311ca41ade1e31
Jack Hubert <jphubert93@gmail.com>
2020-12-06 02:05:30 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -24,9 +24,9 @@ end
 
 def play(songs)
   puts "Please enter a song name or number:"
-  user_input = get_user_input.to_s
+  user_input = get_user_input
   
-  if user_input.between?(1, 9)
+  if user_input.between?("1", "9")
     songs.each_with_index {|song, index|
     index += 1
     
